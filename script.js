cea4f4e667add62b8706b24c259b1f1105c3811c
ling9670 <ling.9670@gmail.com>
2023-05-17 22:49:32 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-script.js
+const bar=document.getElementById('bar');
+const nav=document.getElementById('navbar');
+
+
+if (bar) {
+	bar.addEventListener('click',() => {
+		nav.classList.add('active')
+	})
+
+
+}
